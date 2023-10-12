8cc5f0c3f7ee7e8534ec632844855f820f335ecd
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2023-10-12 16:50:28 -0700

    Implemented animations while skills are in view

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
+import useMediaQuery from "@/app/hooks/useMediaQuery";
+import { motion, Variants } from "framer-motion";
 import { Chivo } from "next/font/google";
 import { Oswald } from "next/font/google";
 
@@ -9,12 +10,53 @@ interface ICON_INTERFACE {
   title: string,
 };
 
-//const SKILLS: string[] = ["Java", "C", "C++", "Python", "Javascript", "HTML", "CSS", "React", "React Native", "Angular", "Node.js", "Express.js", "Spring Boot"];
 //const COURSEWORK: string[] = ["CS31: Object-Oriented Software Development", "CS32: Data Structures and Algorithms", "CS M51A: Digital Systems Design", "CS35L: Software Construction", "CS111: Operating Systems Principles", "CS180: Algorithms and Complexity"];
-//const TECHNOLOGIES: string[] = ["REST API", "Git", "Firebase Services", "Firebase Admin SDK", "MongoDB", "MySQL", "Hibernate", "Microsoft Azure"];
 
-const skillVariants = {
+const skillVariants: Variants = {
+  initial: ({ index, length }) => {
+    let xOffset: string = '0px';
+    let yOffset: string = '0px';
+    if (length % 2 === 1) {
+      if (index < Math.floor(length / 2))
+        xOffset = '-50px';
+      else if (index > Math.floor(length / 2))
+        xOffset = '50px';
+      else
+        yOffset = '50px';
+    } else {
+      if (index < Math.floor(length / 2))
+        xOffset = '-50px';
+      else
+        xOffset = '50px';
+    }
 
+    return {
+      x: xOffset,
+      y: yOffset,
+      opacity: 0,
+    }
+  },
+  animate: ({ index, length }) => {
+    let delay = 0;
+    if (length % 2 === 1) {
+      delay = length - (Math.abs(index - Math.floor(length / 2)));
+    } else {
+      if (index <= (length / 2) - 1)
+        delay = length - (Math.abs(index - ((length / 2) - 1)));
+      else
+        delay = length - (Math.abs(index - ((length / 2))));
+    }
+
+    return {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        ease: "linear",
+        delay: delay * 0.25,
+      }
+    }
+  }
 }
 
 const chivo = Chivo({
@@ -27,39 +69,51 @@ const oswald = Oswald({
   variable: '--font-oswald',
 });
 
-const SkillsIcon = ({src, title}: {src: string, title: string}): JSX.Element => {
+const SkillsIcon = ({src, title, index, length, isAboveMediumScreens}: {src: string, title: string, index: number, length: number, isAboveMediumScreens: boolean}): JSX.Element => {
   return (
-    <div className="flex flex-col items-center transition duration-300 hover:scale-110">
-      <img src={src} height={50} width={50} />
-      <p className={`${oswald.variable} font-oswald text-xl select-none`}>{title}</p>
-    </div>
+    <motion.div 
+      className="flex flex-col items-center transition duration-300 hover:scale-110 select-none"
+      whileHover={{
+        scale: 1.15,
+      }}
+      custom={{index, length}}
+      initial="initial"
+      whileInView="animate"
+      viewport={{ once: true, amount: 0.25 }}
+      variants={skillVariants}
+    >
+      <img src={src} className="h-[45px] w-[45px] sm:h-[55px] sm:w-[55px] md:h-[60px] md:w-[60px] lg:h-[70px] lg:w-[70px]" />
+      <p className={`${oswald.variable} font-oswald text-xl font-light select-none`}>{title}</p>
+    </motion.div>
   );
 }
 
 const index = (): JSX.Element => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  
   return (
-    <section id="skills" className="flex flex-col flex-wrap gap-y-5">
-      <div className="flex flex-col gap-y-2">
-        <h1 className={`${chivo.variable} font-chivo text-2xl`}>SKILLS:</h1>
-        <div className="flex flex-row gap-10">
-          {SKILLS.map((skill) => {
-            return <SkillsIcon src={skill.src} title={skill.title}/>
+    <section id="skills" className="flex flex-col flex-wrap md:gap-y-5">
+      <div className="flex flex-col md:gap-y-2 items-center justify-center">
+        {/*<h1 className={`${chivo.variable} font-chivo text-2xl`}>SKILLS:</h1>*/}
+        <div className="flex flex-row flex-wrap gap-7 md:gap-14 items-center justify-center">
+          {SKILLS.map((skill, index) => {
+            return <SkillsIcon key={index} src={skill.src} title={skill.title} index={index} length={SKILLS.length} isAboveMediumScreens={isAboveMediumScreens}/>
           })}
         </div>
       </div>
-      <div className="flex flex-col gap-y-2">
-        <h1 className={`${chivo.variable} font-chivo text-2xl`}>LIBRARIES AND FRAMEWORKS:</h1>
-        <div className="flex flex-row gap-10">
-          {FRAMEWORKS.map((skill) => {
-            return <SkillsIcon src={skill.src} title={skill.title}/>
+      <div className="flex flex-col md:gap-y-2 items-center justify-center">
+        {/*<h1 className={`${chivo.variable} font-chivo text-2xl`}>LIBRARIES AND FRAMEWORKS:</h1>*/}
+        <div className="flex flex-row flex-wrap gap-7 md:gap-14 items-center justify-center">
+          {FRAMEWORKS.map((skill, index) => {
+            return <SkillsIcon key={index} src={skill.src} title={skill.title} index={index} length={FRAMEWORKS.length} isAboveMediumScreens={isAboveMediumScreens}/>
           })}
         </div>
       </div>
-      <div className="flex flex-col gap-y-2">
-        <h1 className={`${chivo.variable} font-chivo text-2xl`}>TECHNOLOGIES: </h1>
-        <div className="flex flex-row gap-10">
-          {TECHNOLOGIES.map((skill) => {
-            return <SkillsIcon src={skill.src} title={skill.title}/>
+      <div className="flex flex-col md:gap-y-2 items-center justify-center">
+        {/*<h1 className={`${chivo.variable} font-chivo text-2xl`}>TECHNOLOGIES: </h1>*/}
+        <div className="flex flex-row flex-wrap gap-7 md:gap-14 items-center justify-center">
+          {TECHNOLOGIES.map((skill, index) => {
+            return <SkillsIcon key={index} src={skill.src} title={skill.title} index={index} length={TECHNOLOGIES.length} isAboveMediumScreens={isAboveMediumScreens}/>
           })}
         </div>
       </div>
