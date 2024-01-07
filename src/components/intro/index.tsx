aec221d1b92ecead8075d0a8d9632218550ab793
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2024-01-07 15:53:36 -0800

    Updated resume to 01-01-24

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -114,7 +114,7 @@ const Index = (): JSX.Element => {
           </a>
           <a
             className="hover:opacity-50 transition duration-250 -ml-[0.15rem]"
-            href="/Resume123023.pdf"
+            href="/Resume010124.pdf"
             target="_blank"
             rel="noreferrer"
           >
